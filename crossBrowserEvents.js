272690d97fb1af834d07886018a7ad8eff31f26a
Christopher McClurg <christopher.mcclurg@gmail.com>
2016-07-14 17:55:50 -0400

    adding lessons from middle of the book

---
+++ crossBrowserEvents.js
@@ -0,0 +1,47 @@
+var eventUtility = {
+	addEvent : (function(){
+		if (typeof addEventListener !== "undefined"){
+			return function(obj, evt, fn) {
+				obj.addEventListener(evt, fn, false);
+			};
+		} else {
+			return function(obj,evt,fn){
+				obj.attachEvent("on" + evt, fn);
+			};
+		}
+	}()),	
+	removeEvent : (function(){
+		if (typeof removeEventListener !== "undefined"){
+			return function(obj, evt, fn) {
+				obj.removeEventListener(evt, fn, false);
+			};
+		} else {
+			return function(obj,evt,fn){
+				obj.detachEvent("on" + evt, fn);
+			};
+		}
+	}()),
+	getTarget : (function (){
+		if (typeof addEventListener !== "undefined"){
+			return function(event){
+				return event.target;
+			};
+		} else {
+			return function(event) {
+				return event.srcElement;
+			};
+		}
+	}()),
+	preventDefault : (function() {
+		if (typeof addEventListener !== "undefined") {
+			return function(evt) {
+				event.preventDefault();
+			};
+		} else {
+			return function(evt) {
+				event.returnValue = false;
+			};
+		}
+	}())	
+};
+
